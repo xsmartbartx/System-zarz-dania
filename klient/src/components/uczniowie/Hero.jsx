9978144affb16b193d34082f3c065ca2410d7240
xsmartbartx <bw141105@gmail.com>
2025-02-26 18:37:25 +0100

    Update Hero.jsx

--- klient/src/components/uczniowie/Hero.jsx
+++ klient/src/components/uczniowie/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Hero = () => {
   return (
-    <div><h1>Hero</h1></div>
+    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 sapce-y-7 text-center bg-gradient-to-b fraom-cyan-100/70'>
+      <h1 className='md: text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3x1 mx-auto'>Corses from fields related to
+      <span className='text-blue-600'>IT</span><img src={assets.sketch} alt="sketch"
+      className='md:block hidden absolute -bottom-7 right-0'/></h1>
+
+      <p className='md:block hidden text-gray-500 max-w-2x1 mx-auto'></p>
+
+      <p className='md:block hidden text-gray-500 max-w-2x1 mx-auto'></p>
+    </div>
   )
 }
 
