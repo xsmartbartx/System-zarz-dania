8679dd4d5de22e9d2c0ddda6785cb42a3d560b93
xsmartbartx <bw141105@gmail.com>
2025-02-26 18:07:33 +0100

    Update Navbar.jsx

--- klient/src/components/uczniowie/Navbar.jsx
+++ klient/src/components/uczniowie/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
   const isCourseListPage = location.pathname.includes ('/course-list'); 
+
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
   return (
     <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14
@@ -13,18 +17,30 @@ const Navbar = () => {
       <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
           <div className='flex items-center gap-5'>
-            <button>Zostań Nauczcycielem</button>
+            {user && <>
+              <button>Zostań Nauczcycielem</button>
             <Link to='/zapisy'>Zapisy</Link>
+            </>
+            }
           </div>
-          <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Stwórz konto</button>
+
+          {user ? <UserButton /> :
+          <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Stwórz konto</button>}
       </div>
       <div className='md:hidden flex items-center gap-3 sm:gap-5 text-gray-500'>
-        <div>
-          <button>Zostań Nauczycielem</button>
-            <Link to='/zapisy'>Moje Zapisy</Link>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+          {user && 
+            <>
+                <button>Zostań Nauczycielem</button>
+              <Link to='/zapisy'>Moje Zapisy</Link>
+            </>
+            }
         </div>
-        <button><img src={assets.user_icon} alt="" /></button>
-      </div>
+        {
+          user ? <UserButton /> 
+          : <button onClick={()=> openSignIn()}><img src={assets.user_icon} alt="" /></button>
+        }
+        </div>
     </div>
   )
 }
