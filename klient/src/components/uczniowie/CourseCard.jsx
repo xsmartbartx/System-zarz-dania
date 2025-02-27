5997046c7cb96a58a020c1835c23b658fb790e82
xsmartbartx <bw141105@gmail.com>
2025-02-27 22:20:13 +0100

    v1.1.0.9

--- klient/src/components/uczniowie/CourseCard.jsx
+++ klient/src/components/uczniowie/CourseCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
-const CourseCard = () => {
+const CourseCard = ({course}) => {
   return (
-    <div><h1>CourseCard</h1></div>
+    <div>
+      <img src={course.courseThumbnail} alt="" className='w-full h-60 object-cover rounded-lg' />
+      <div>
+        <h3>{course.CourseTitle}</h3>
+        <p>{course.nauczyciel.name}</p>
+        <div>
+          <p>3.5</p>
+          <div>
+            {[...Array(5)].map((_, i)=>(<img key={i} src={assets.star} alt='' />
+            ))}
+          </div>
+          <p>23</p>
+        </div>
+        <p>{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
+      </div>
+    </div>
   )
 }
 
