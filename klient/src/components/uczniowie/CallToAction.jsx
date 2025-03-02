9d80d7a1c22452df01f813782a2a747f13d83953
xsmartbartx <bw141105@gmail.com>
2025-03-02 22:31:43 +0100

    Update CallToAction.jsx

--- klient/src/components/uczniowie/CallToAction.jsx
+++ klient/src/components/uczniowie/CallToAction.jsx
@@ -3,10 +3,10 @@ import { assets } from '../../assets/assets'
 
 const CallToAction = () => {
   return (
-    <div>
-      <h1>CallToAction</h1>
-      <p></p>
-      <div>
+    <div className='flex flex-col items-center space-y-5 text-center py-10'>
+      <h1 className='text-x1 md:text-4x1 text-gray-800 font-semibold'>CallToAction</h1>
+      <p className='text-gray-500 sm:text-sm'></p>
+      <div className='flex flex-col items-center space-y-5 md:flex-row md:space-x-5 md:space-y-0'>
         <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Zacznij</button>
         <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Dowiedz się więcej<img src={assets.arrow_icon} alt='arrow_icon' /></button>
     
