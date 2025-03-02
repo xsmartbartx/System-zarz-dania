763d37bed67f91ddc2d8778098c6c2183a182317
xsmartbartx <bw141105@gmail.com>
2025-03-02 22:38:01 +0100

    Update Footer.jsx

--- klient/src/components/uczniowie/Footer.jsx
+++ klient/src/components/uczniowie/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div><h1>Footer</h1></div>
+    <footer className='flex flex-col items-center space-y-5 text-center py-10'>
+      <div className='flex flex-col items-center space-y-5'>
+        <div className='flex items-center gap-5'>
+          <img src={assets.logo_dark} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
+          <p></p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <p></p>
+    </footer>
   )
 }
 
