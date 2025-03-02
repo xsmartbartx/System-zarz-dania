62d2ba1529624339fc0f575173f016696eb62662
xsmartbartx <bw141105@gmail.com>
2025-03-02 22:16:16 +0100

    Update CallToAction.jsx

--- klient/src/components/uczniowie/CallToAction.jsx
+++ klient/src/components/uczniowie/CallToAction.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const CallToAction = () => {
   return (
-    <div><h1>CallToAction</h1></div>
+    <div>
+      <h1>CallToAction</h1>
+      <p></p>
+      <div>
+        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Zacznij</button>
+        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Dowiedz się więcej<img src={assets.arrow_icon} alt='arrow_icon' /></button>
+    
+      </div>
+    </div>
   )
 }
 
