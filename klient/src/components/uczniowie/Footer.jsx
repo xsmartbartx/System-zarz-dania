b66dcb2c79cc49d6ce8200290839c064d754d18f
xsmartbartx <bw141105@gmail.com>
2025-03-03 20:40:42 +0100

    Update Footer.jsx

--- klient/src/components/uczniowie/Footer.jsx
+++ klient/src/components/uczniowie/Footer.jsx
@@ -7,12 +7,25 @@ const Footer = () => {
       <div className='flex flex-col items-center space-y-5'>
         <div className='flex items-center gap-5'>
           <img src={assets.logo_dark} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
-          <p></p>
+          <p className='mt-6 text-center md:text-lefty text-sm text-white/80'></p>
         </div>
-        <div></div>
-        <div></div>
+        <div className='flex items-center gap-5'>
+          <h2 className='font-semibold text-white mb-5'>Firma</h2>
+          <ul className='flex flex-col items-center gap-2'>
+            <li><a href='#'>Główna</a></li>
+            <li><a href='#'>O nas</a></li>
+            <li><a href='#'>Skontaktuj się</a></li>
+            <li><a href='#'>Regulamin użytkowania</a></li>
+          </ul>
+        </div>
+        <div className='hidden md:flex flex-col items-center gap-5'>
+          <h2 className='font-semibold text-white mb-5'>Kliknij po Newsletter</h2>
+          <p className='text-sm text-white/80'></p>
+        </div>
+          <input type='email' placeholder='Wpisz swój email' className='bg-gray-800 text-white px-5 py-2 rounded-full' />
+          <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Zapisz się</button>
       </div>
-      <p></p>
+      <p className='py-4 text-center text-xs md:text-sm text-white/60'></p>
     </footer>
   )
 }
