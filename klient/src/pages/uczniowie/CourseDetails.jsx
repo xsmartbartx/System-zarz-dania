150b836edabd27c27565ac95b19159306a927ddc
xsmartbartx <bw141105@gmail.com>
2025-03-03 21:32:35 +0100

    Update CourseDetails.jsx

--- klient/src/pages/uczniowie/CourseDetails.jsx
+++ klient/src/pages/uczniowie/CourseDetails.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const CourseDetails = () => {
+
+  const {id} = useParams();
+
+  const [courseData, setCourseData] = useState(null);
+
+  const {allCourses} = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find(course => course._id === id)
+    setCourseData(findCourse);
+  }
+
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+
   return (
-    <div><h1>CourseDetails</h1></div>
+    <div className='course-details'>
+      
+      <div className='absolute top-0 left-0 w-full h-section-height'></div>
+
+      <div></div>
+
+      <div></div>
+
+    </div>
   )
 }
 
