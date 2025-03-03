a43d26d426734912470d91699cb5232093396f17
xsmartbartx <bw141105@gmail.com>
2025-03-03 21:35:21 +0100

    Update tailwind.config.js

--- klient/tailwind.config.js
+++ klient/tailwind.config.js
@@ -4,15 +4,20 @@ export default {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      'course-details-heading-small' : ['26px', '36px'],
-      'course-details-heading-large' : ['36px', '44px'],
-      'home-heading-small' : ['28px', '34px'],
-      'home-heading-large' : ['48px', '56px'],
-      'default': ['15px', '21px'],
-    },
-    gridTemplateColumns: {
-      'auto': 'repeat(auto-fill, minmax(250px, 1fr))',
-      'course-card': 'repeat(auto-fill, minmax(250px, 1fr))',
+      fontSize: {
+        'course-details-heading-small' : ['26px', '36px'],
+        'course-details-heading-large' : ['36px', '44px'],
+        'home-heading-small' : ['28px', '34px'],
+        'home-heading-large' : ['48px', '56px'],
+        'default': ['15px', '21px'],
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(250px, 1fr))',
+        'course-card': 'repeat(auto-fill, minmax(250px, 1fr))',
+      },
+      spacing: {
+        'section-height': '500px',
+      },
     },
   },
   plugins: [],
