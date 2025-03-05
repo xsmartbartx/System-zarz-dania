99013a9d68ced5d35b3a9174e417d0f3db7bf8ed
xsmartbartx <bw141105@gmail.com>
2025-03-05 23:52:27 +0100

    Update CourseDetails.jsx

--- klient/src/pages/uczniowie/CourseDetails.jsx
+++ klient/src/pages/uczniowie/CourseDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
+import Ładowanie from '../../components/Ładowanie'
 
 const CourseDetails = () => {
 
@@ -20,17 +21,22 @@ const CourseDetails = () => {
     fetchCourseData();
   }, []);
 
-  return (
+  return courseData ? (
+    <>
     <div className='course-details'>
       
       <div className='absolute top-0 left-0 w-full h-section-height'></div>
 
-      <div></div>
+      <div>
+        <h1>{courseData.courseTitle}</h1>
+        <p>{courseData.coursedescription}</p>
+      </div>
 
       <div></div>
 
     </div>
-  )
+    </>
+  ) : <Ładowanie />;
 }
 
 export default CourseDetails
