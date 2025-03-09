cad3d53416c761868a440404be754cdd3d443a1c
xsmartbartx <bw141105@gmail.com>
2025-03-09 21:48:47 +0100

    Update tailwind.config.js

--- klient/tailwind.config.js
+++ klient/tailwind.config.js
@@ -18,6 +18,12 @@ export default {
       spacing: {
         'section-height': '500px',
       },
+      maxWidth: {
+        'course-card' : '424px'
+      },
+      boxShadow: {
+        'custom-card': '0px 4px 15px 2px rgba(0, 0, 0, 0.1)'
+      }
     },
   },
   plugins: [],
