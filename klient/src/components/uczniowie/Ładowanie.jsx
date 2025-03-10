bac2d572e88bfb7e0157e10c1fe069bebcfae370
xsmartbartx <bw141105@gmail.com>
2025-03-10 21:27:23 +0100

    Update Ładowanie.jsx

--- klient/src/components/uczniowie/Ładowanie.jsx
+++ klient/src/components/uczniowie/Ładowanie.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Ładowanie = () => {
   return (
-    <div><h1>Ładowanie</h1></div>
+    <div>
+      <div className='w-16 sm:w-20 aspect-square border-4 border-gray-300
+       border-t-4 border-t-blue-400 rounded-full animate-spin animate-spin-slow'>
+      </div>
+    </div>
   )
 }
 
