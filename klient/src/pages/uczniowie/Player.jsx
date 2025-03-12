8976ddda80fd8b96d54a3c49b2ce2a6c02c3e726
xsmartbartx <bw141105@gmail.com>
2025-03-12 22:47:33 +0100

    Update Player.jsx

--- klient/src/pages/uczniowie/Player.jsx
+++ klient/src/pages/uczniowie/Player.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Player = () => {
   return (
-    <div><h1>Osoba</h1></div>
+<>
+    <div className='p-4 sm:p-10 flex flex-col-reverse md:flex-row'>
+      <div className='container mx-auto p-4'>
+        <h2 className='text-x1 font-semibold'>Osoba</h2>
+      </div>
+
+
+    <div></div>
+
+    </div>
+</>
   )
 }
 
