1981f721557e110213c62eb680a811d02f5043b0
xsmartbartx <bw141105@gmail.com>
2025-03-13 21:59:08 +0100

    Update SystemOceniania.jsx

--- klient/src/components/uczniowie/SystemOceniania.jsx
+++ klient/src/components/uczniowie/SystemOceniania.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const SystemOceniania = () => {
+
+  const [rating, setRating] = useState(initialSystemOceniania || 0);
+
+  const handleSystemOceniania = (value) => {
+    setS
+
   return (
     <div>
       {Arrayl.from({length: 5}).map((_, index) => 
       const starValue = index + 1;
       return (
-        <span key={index}>
+        <span key={index} className={`text-x1 sm:text-2x1 cursor-pointer
+         ${starValue <= SystemOceniania}`}>
 
         </span>
       )
