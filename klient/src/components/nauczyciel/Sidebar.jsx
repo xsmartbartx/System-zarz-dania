97d1d0b07d77304e8fce7f082788c030e547e2ed
xsmartbartx <bw141105@gmail.com>
2025-03-13 22:47:51 +0100

    Update Sidebar.jsx

--- klient/src/components/nauczyciel/Sidebar.jsx
+++ klient/src/components/nauczyciel/Sidebar.jsx
@@ -18,7 +18,14 @@ const Sidebar = () => {
     <div className='md:w-64 w-16 border-r min-h-screen text=base border-gray-500
      py-2 flex flex-col'>
       {menuItems.map((item)=>{
-        <NavLink>
+        <NavLink
+        to={item.path}
+        key={item.name}
+        end={item.path === '/nauczyciel'}
+        className={({isActive})=> `flex items-center md:flex-row flex-col
+         md:justify-start justify-center py-3.5 md:px-10 gap-3 ${isActive ?
+          'bg-indigo-50 border-r-[6px] border-indigo-500/90' :
+           'hover:bg-gray-100/90 border-r-[6px] border-white hover:border-gray'}`}>
           <img src={item.icon} alt="" className='w-6 h-6' />
           <p className='md:block hidden text-center'>{item.name}</p>
         </Navlink>
