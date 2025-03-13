bc6139d8294f73232ee993d0386fdce9e5807d34
xsmartbartx <bw141105@gmail.com>
2025-03-13 22:38:02 +0100

    Update Sidebar.jsx

--- klient/src/components/nauczyciel/Sidebar.jsx
+++ klient/src/components/nauczyciel/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React, { use, useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
 import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
 
-  const { isEducator } = useContext(AppContext)
+  const { isNauczyciel } = useContext(AppContext)
 
   const menuItems = [
     {name: 'Panel', path: '/nauczyciel', icon:assets.home_icon},
@@ -13,9 +14,15 @@ const Sidebar = () => {
     {name: 'ZapisanyUczeń', path: '/nauczyciel/uczen-zapisany', icon:assets.home_person_tick_icon},
   ]
 
-  return (
-    <div>
-      <h1>Sidebar</h1>
+  return isNauczyciel && (
+    <div className='md:w-64 w-16 border-r min-h-screen text=base border-gray-500
+     py-2 flex flex-col'>
+      {menuItems.map((item)=>{
+        <NavLink>
+          <img src={item.icon} alt="" className='w-6 h-6' />
+          <p className='md:block hidden text-center'>{item.name}</p>
+        </Navlink>
+      })}
     </div>
   )
 }
