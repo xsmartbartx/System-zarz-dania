5de83ab6eee20824d9e76aa98d86b032cbb80d48
xsmartbartx <bw141105@gmail.com>
2025-03-13 22:14:19 +0100

    Update Navbar.jsx

--- klient/src/components/nauczyciel/Navbar.jsx
+++ klient/src/components/nauczyciel/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { assets, dummyEducatorData } from '../../assets/assets';
+import { userButton, useUser } from '@clerk/clerk-react';
+import { Link } from  'react-router-dom';
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData
+  const { user } = useUser()
+
   return (
-    <div><h1>Navbar</h1></div>
+    <div>
+      <Link to='/'>
+      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32' />
+      </Link>
+    </div>
   )
 }
 
