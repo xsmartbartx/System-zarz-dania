f5fdbd6ec56d9251da71b91c20c78661e5786c04
xsmartbartx <bw141105@gmail.com>
2025-03-13 22:39:39 +0100

    Update Nauczyciel.jsx

--- klient/src/pages/nauczyciel/Nauczyciel.jsx
+++ klient/src/pages/nauczyciel/Nauczyciel.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/nauczyciel/Navbar'
+import Sidebar from '../../components/nauczyciel/Sidebar'
 
 const Nauczyciel = () => {
   return (
     <div>
       <Navbar />
       <div>
-          {<Outlet />}
+        <Sidebar />
+        <div>
+            {<Outlet />}
+        </div>
       </div>
     </div>
   )
