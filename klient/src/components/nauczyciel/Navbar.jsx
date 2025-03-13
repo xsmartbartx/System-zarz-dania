82d2675355f6ba640d0313a9a28364062583ae3f
xsmartbartx <bw141105@gmail.com>
2025-03-13 22:18:38 +0100

    Update Navbar.jsx

--- klient/src/components/nauczyciel/Navbar.jsx
+++ klient/src/components/nauczyciel/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { assets, dummyEducatorData } from '../../assets/assets';
-import { userButton, useUser } from '@clerk/clerk-react';
+import { UserButton, userButton, useUser } from '@clerk/clerk-react';
 import { Link } from  'react-router-dom';
 
 const Navbar = () => {
-  const educatorData = dummyEducatorData
+  const NauczycielData = dummyEducatorData
   const { user } = useUser()
 
   return (
@@ -12,6 +12,10 @@ const Navbar = () => {
       <Link to='/'>
       <img src={assets.logo} alt="Logo" className='w-28 lg:w-32' />
       </Link>
+      <div className='flex items-center gap-5'>
+        <p>Yo! {user ? user.fullName : 'Developers'}</p>
+        {user ? <UserButton /> : <img className='max-w-8' src={assets.profile_img} />}
+      </div>
     </div>
   )
 }
