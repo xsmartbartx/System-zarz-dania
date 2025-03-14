fae6b9654a8e6d9c34b160635866697e5d2caa48
xsmartbartx <bw141105@gmail.com>
2025-03-14 21:57:24 +0100

    Update Panel.jsx

--- klient/src/pages/nauczyciel/Panel.jsx
+++ klient/src/pages/nauczyciel/Panel.jsx
@@ -34,6 +34,36 @@ const Panel = () => {
             </div>
           </div>
         </div>
+        <div>
+          <h2 className='pb-4 text-lg font-medium'>Ostatnie zapisy</h2>
+          <div className='flex flex-col items-center max-w-4x1 w-full overflow-hidden
+           rounded-md bg-white border border-gray-500/20'>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nazwa Ucznia</th>
+                <th>Nazwa Kursu</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dashboardData.zapisanuUczniowieData.map((item, index) => (
+                <tr key={index}></tr>
+                <td>{index +1}</td>
+                <td>
+                  <img src={item.student.imageUrl} alt="Profil" />
+                  <span>{item.student.name}</span>
+                </td>
+                <td>{item.courseTitle}</td>
+              ))}
+            </tbody>
+          </table>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     </div>
   ) : <Ładowanie />
