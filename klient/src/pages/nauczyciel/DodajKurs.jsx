1f3fc35c65191a91baad7d069788f884c8b2d7e6
xsmartbartx <bw141105@gmail.com>
2025-03-14 22:59:15 +0100

    Update DodajKurs.jsx

--- klient/src/pages/nauczyciel/DodajKurs.jsx
+++ klient/src/pages/nauczyciel/DodajKurs.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import uniqid from 'unigid';
+import Quill from 'quill';
 
 const DodajKurs = () => {
+
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] = useState('')
+  const [coursePrice, setCoursePrice] = useState(0)
+  const [discount, setDiscount] = useState(0)
+  const [image, setImage] = useState(null)
+  const [chapters, setChapters] = useState([]);
+  const [currentChapterId, setCurrentChapterId] = useState(null);
+
+  const [lecturedetails, setLectureDetails] = useState(
+    {
+      lectureTitle: '',
+      lectureDuration: '',
+      lectureUrl: '',
+      isPreviewFree: false,
+    }
+  )
+
+  useEffect(()=>{
+    if(!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: 'snow',
+      })
+    }
+  }, [])
+
   return (
     <div><h1>DodajKurs</h1></div>
   )
