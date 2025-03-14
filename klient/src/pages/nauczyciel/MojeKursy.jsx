746028b1d2d63d8ceddd4e48495969b6d748dd0c
xsmartbartx <bw141105@gmail.com>
2025-03-14 22:04:01 +0100

    Update MojeKursy.jsx

--- klient/src/pages/nauczyciel/MojeKursy.jsx
+++ klient/src/pages/nauczyciel/MojeKursy.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const MojeKursy = () => {
+
+  const {currency, allCourses} = useContext(AppContext)
+  
+  const [courses, setCourses] = useState(null)
+
+  const fetchNauczycielCourses = async () => {
+    setCourses(allCourses)
+  }
+
+  useEffect(() => {
+    fetchNauczycielCourses()
+  },[])
+
   return (
     <div><h1>MojeKursy</h1></div>
   )
