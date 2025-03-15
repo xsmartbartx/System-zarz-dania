2da826a392918f2ad4fd0d702540650b56c5661d
xsmartbartx <bw141105@gmail.com>
2025-03-15 15:44:13 +0100

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        _id: {type: String, required: true},
+        name: {type: String, required: true},
+        email: {type: String, required: true},
+        imageUrl: {type: String, required: true},
+        enrolledCourses: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Course'
+            }
+        ],
+    }, {}
+)
