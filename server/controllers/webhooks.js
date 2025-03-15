165cf45d7a83b6d25ce8dd8f526c075a8fb18094
xsmartbartx <bw141105@gmail.com>
2025-03-15 16:10:34 +0100

    Update webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,8 +3,17 @@ import User from "../models/User.js";
 
 const clerkWevhooks = async (req, res)=> {
     try {
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+
+        const
 
     } catch (error) {
-        
+
     }
 }
