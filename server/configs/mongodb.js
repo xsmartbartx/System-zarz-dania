0d859a23d94edeb312b341ec32fa14a61b030295
xsmartbartx <bw141105@gmail.com>
2025-03-15 15:30:49 +0100

    Update mongodb.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,4 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     mongoose.connection.on('connected', ()=> console.log(`Baza połączona`))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
 }
+
+export default connectDB
