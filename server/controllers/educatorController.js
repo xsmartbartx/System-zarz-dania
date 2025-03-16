3706e72949c20ecc0693d21cac9d8f3c0cd09848
xsmartbartx <bw141105@gmail.com>
2025-03-16 23:09:49 +0100

    Update educatorController.js

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,5 @@
 import { clerkClient } from '@clerk/express'
+import Course from '../models/Course'
 
 export const updateRoleToEducator = async (req, res)=> {
     try {
@@ -13,5 +14,25 @@ export const updateRoleToEducator = async (req, res)=> {
         res.json({ success: true, message: 'Możesz kuż opublikować kurs' })
     } catch (error) {
         res.json({ success: false, message: error.message })
+    }
+}
+
+export const addCourse = async (req, res)=>{
+    try {
+        const { courseData } = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+
+        if(!imageFile){
+            return res.json({ success: false, message: 'Plik nie załączony'})
+        }
+
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+
+    } catch (error) {
+
     }
 }
