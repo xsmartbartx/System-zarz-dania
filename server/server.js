fe3b3ea907a428baa9b75dc7d9ba842a78561a6d
xsmartbartx <bw141105@gmail.com>
2025-03-16 21:48:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,18 +3,31 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educator.Routes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
 
 await connectDB()
 
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get('/', (req, res)=> res.send("API Pracuje"))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
+const server = app.listen(PORT, ()=>{
     console.log(`Serwer pracuje na porcie ${PORT}`)
 })
+
+server.on('error', (error) => {
+    if (error.code === 'EADDRINUSE') {
+        console.error(`Port ${PORT} is already in use.`);
+        process.exit(1);
+    } else {
+        throw error;
+    }
+});
