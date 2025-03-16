8559028fa93bba231058b8995466c76c9b0bf48f
xsmartbartx <bw141105@gmail.com>
2025-03-16 01:56:42 +0100

    Create educatorController.js

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,11 @@
+import { clerkClient } from '@clerk/express'
+
+export const updateRoleToEducator = async ()=> {
+    try {
+        const userId = requestAnimationFrame.auth.userId
+
+        await clerkClient
+    } catch (error) {
+        
+    }
+}
