73740b7fd9ee996d6714664462afb18d0c6688da
xsmartbartx <bw141105@gmail.com>
2025-03-16 22:35:19 +0100

    Update Course.js

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
+const chapterSchema = new mongoose.Schema({
+    chapterId: { type: String, required: true },
+    chapterOrder: { type: Number, required: true },
+    chapterTitle: { type: String, required: true },
+    chapterContent: [lectureSchema]
+}, {_id: false});
+
 const courseSchema = new mongoose.Schema({
     courseTitle: { type: String, required: true},
     courseDescription: { type: String, required: true},
@@ -11,4 +18,8 @@ const courseSchema = new mongoose.Schema({
     courseRatings:[
         { userId: { type: String }, rating: { type: Number, min: 1, max: 5 }}
     ],
-})
+    educator: { type: String, ref: 'User', required: true },
+    enrolledStudents: [
+        {type: String, ref: 'User' }
+    ],
+}, {timestamps: true, minimize: fasle})
