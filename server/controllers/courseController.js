b88b50ccd98bc004095472c35f57d2f1a79f17e8
xsmartbartx <bw141105@gmail.com>
2025-03-17 23:14:12 +0100

    Update courseController.js

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -4,7 +4,23 @@ export const getAllCourse = async (req, res)=>{
     try {
         const courses = await Course.find({isPublished: true}).select
         (['-courseContent', '-enrolledStudents']).populate({path: 'educator'})
+
+        res.json({ success: true, courses })
     } catch (error) {
-        
+        res.json({ success: false, message: error.message })
     }
 } 
+
+export const getCourseId = async (req, res)=>{
+    const {id} = req.params
+
+    try {
+        const courseData = await Course.findById(id).populate({path: 'educator'})
+
+        courseData.courseContent.forEach(chapter => {
+            chapter
+        })
+    } catch (error) {
+
+    }
+}
