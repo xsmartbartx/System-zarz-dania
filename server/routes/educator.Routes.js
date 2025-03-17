030dcdf75138a1bdb08c5310f5a61fad596c453f
xsmartbartx <bw141105@gmail.com>
2025-03-17 21:11:47 +0100

    Update educator.Routes.js

--- server/routes/educator.Routes.js
+++ server/routes/educator.Routes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import {updateRoleToEducator} from '../controllers/educatorController.js'
+import upload from '../configs/multer.js'
+import { protectEducator} from '../middlewares/authMidlleware.js'
 
 const educatorController = express.Router()
 
 educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', upload.single('image'), protectEducator)
 
 export default educatorRouter;
