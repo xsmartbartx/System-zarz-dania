41aee77ea020d60f2ab3b7364c88998fd4393f67
xsmartbartx <bw141105@gmail.com>
2025-03-19 23:24:04 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import educatorRouter from './routes/educator.Routes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 
@@ -20,6 +21,7 @@ app.get('/', (req, res)=> res.send("API Pracuje"))
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
 
 const PORT = process.env.PORT || 5000
 
