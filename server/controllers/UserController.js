69e5dde9436e9f5d3709bb5354983921ee262d6b
xsmartbartx <bw141105@gmail.com>
2025-03-19 23:52:31 +0100

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -26,3 +26,30 @@ export const userEnrolledCourses = async (req, res)=>{
         res.json({ success: false, message: error.message })
     }
 }
+
+export const purchaseCourse = async (req, res)=>{
+    try{
+        const { courseId } = req.body
+        const { origin } = req.headers
+        const userId = req.auth.userId
+        const userData = await User.findById(userId)
+        const courseData = await Course.findById(courseId)
+
+        if(!userData || !courseData){
+            return res.json({ success: false, message: 'Dane nie znalezione'})
+        }
+
+        const purchaseData = {
+            courseId: courseData._id,
+            userId,
+            amount: (courseData.coursePrice - courseData.discount *
+             courseData.coursePrice / 100).toFixed(2),
+        }
+
+        const newPurchase = await Purchase.create(purchaseData)
+
+        const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+    } catch (error) {
+
+    }
+}
