6746285cb056766b0e149e1324c9a43f751fb071
xsmartbartx <bw141105@gmail.com>
2025-03-19 23:49:26 +0100

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -26,21 +26,3 @@ export const userEnrolledCourses = async (req, res)=>{
         res.json({ success: false, message: error.message })
     }
 }
-
-export const purchaseCourse = async (req, res)=>{
-    try{
-        const { courseId } = req.body
-        const { origin } = req.headers
-        const userId = req.auth.userId
-        const userData = await User.findById(userId)
-        const courseData = await Course.findById(courseId)
-
-        if(!userData || !courseData){
-            return res.json({ success: false, message: 'Dane nie znalezione'})
-        }
-
-
-    } catch (error) {
-
-    }
-}
