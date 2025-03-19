a8fdcef78809909713ec4dffa9e733a827478be9
xsmartbartx <bw141105@gmail.com>
2025-03-19 22:03:07 +0100

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,8 +1,28 @@
+import { Message } from "svix/dist/api/message"
+import User from "../models/User.js"
+
 export const getUserData = async (req, res)=>{
     try {
         const userId = req.auth.userId
         const User = await User.findById(userId)
-    } catch (error){
-        
+
+        if(!user){
+            return res.json({ success: false, message: 'Użytkownik nie znaleziony'})
+        }
+
+        req.json({ success: true, user })
+    } catch (error) {
+        req.json({ success: false, message: error.message })
+    }
+}
+
+export const userEnrolledCourses = async (req, res)=>{
+    try {
+        const userId = req.auth.userId
+        const userData = await User.findById(userId).populate('enrolledCourses')
+
+        res.json({success: true, enrolledCourses: userData.enrolledCourses})
+    } catch (error) {
+        res.json({ success: false, message: error.message })
     }
 }
