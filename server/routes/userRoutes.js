4f9ffc7d62572c5f023c8e62b954e3c497f35856
xsmartbartx <bw141105@gmail.com>
2025-03-20 19:04:43 +0100

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { getUserData, userEnrolledCourses } from '../controllers/UserController.js'
+import { purchaseCourse } from '../controllers/courseController.js'
 
 const userRouter = express.Router()
 
 userRouter.get('/data', getUserData)
 userRouter.get('enrolled-courses', userEnrolledCourses)
+userRouter.get('/purchase', purchaseCourse)
 
 export default userRouter;
