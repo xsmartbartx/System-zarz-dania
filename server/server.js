1ad80ec9a47cbdc8b3e5b0b2d1bd458655951084
xsmartbartx <bw141105@gmail.com>
2025-03-20 20:25:59 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educator.Routes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
@@ -22,6 +22,7 @@ app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
+app.post('stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 const PORT = process.env.PORT || 5000
 
