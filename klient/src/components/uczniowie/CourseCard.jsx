479561bc69f7a47b68d6ab8f5bded6f8e9819475
xsmartbartx <bw141105@gmail.com>
2025-03-23 14:00:43 +0100

    Update CourseCard.jsx

--- klient/src/components/uczniowie/CourseCard.jsx
+++ klient/src/components/uczniowie/CourseCard.jsx
@@ -15,7 +15,7 @@ const CourseCard = ({course}) => {
        src={course.courseThumbnail} alt="" />
       <div className='flex flex-col items-center space-y-2'>
         <h3 className='text-base font-semibold'>{course.CourseTitle}</h3>
-        <p className='text-gray-500'>MiejskiSurfer</p>
+        <p className='text-gray-500'>{course.educator.name}</p>
         <div className='flex items-center space-x-2'>
           <p>{calculateRating(course)}</p>
           <div className='flex space-x-1'>
