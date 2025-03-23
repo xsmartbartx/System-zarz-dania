83016350853822278764df1d7f05c11ee565796b
xsmartbartx <bw141105@gmail.com>
2025-03-23 19:16:41 +0100

    Update Player.jsx

--- klient/src/pages/uczniowie/Player.jsx
+++ klient/src/pages/uczniowie/Player.jsx
@@ -64,6 +64,23 @@ const Player = () => {
     }
   }
 
+  const getCourseProgress = async ()=>{
+    try {
+      const token = await getToken()
+      const { data } = await axios.post(backendUrl + '/api/user/get-course-progress',
+        {courseId}, { headers: { Autorization: `Bearer ${token}`}})
+
+        if (data.success){
+          toast.success(data.message)
+          getCourseProgress()
+        }else{
+          toast.error(data.message)
+        }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
 <>
     
