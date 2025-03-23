6a5084c4066505cb892d2f4ed23a7432e85e5a90
xsmartbartx <bw141105@gmail.com>
2025-03-23 22:52:07 +0100

    Update Dashboard.jsx

--- klient/src/pages/educator/Dashboard.jsx
+++ klient/src/pages/educator/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from 'react'
 import { AppContext } from '../../context/AppContext'
 import { assets, dummyDashboardData } from '../../assets/assets'
+import Loading from '../../components/student/Loading'
 
 const Dashboard = () => {
 
