322ec7bddca9f7c7c86522212524f66fdfe0ec5c
xsmartbartx <bw141105@gmail.com>
2025-03-23 14:42:05 +0100

    Update Loading.jsx

--- klient/src/components/uczniowie/Loading.jsx
+++ klient/src/components/uczniowie/Loading.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Loading = () => {
+
+  const { path } = useParams()
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(path){
+      const timer = setTimeout(()=>{
+        navigate(`/${path}`)
+      }, 5000)
+      return ()=> clearTimeout(timer);
+    }
+  },[])
   return (
     <div>
       <div className='w-16 sm:w-20 aspect-square border-4 border-gray-300
