bdaf168847dff92d96af911c5b23e3e341b21363
xsmartbartx <bw141105@gmail.com>
2025-03-23 14:37:59 +0100

    Create Loading.jsx

---
+++ klient/src/components/uczniowie/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      <div className='w-16 sm:w-20 aspect-square border-4 border-gray-300
+       border-t-4 border-t-blue-400 rounded-full animate-spin animate-spin-slow'>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
