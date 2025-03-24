5dd4d32a799521589f444a2b04477c81c23029a2
xsmartbartx <bw141105@gmail.com>
2025-03-24 18:47:10 +0100

    Update Main.jsx

--- klient/src/pages/students/Main.jsx
+++ klient/src/pages/students/Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Hero from '../../components/uczniowie/Hero'
-import Companies from '../../components/uczniowie/Companies'
-import CourseSection from '../../components/uczniowie/CourseSection'
-import Refferentials from '../../components/uczniowie/Referencje'
-import CallToAction from '../../components/uczniowie/CallToAction'
-import Footer from '../../components/uczniowie/Footer'
+import Hero from '../../components/students/Hero'
+import Companies from '../../components/students/Companies'
+import CourseSection from '../../components/students/CourseSection'
+import Refferentials from '../../components/students/Refferentials'
+import CallToAction from '../../components/students/CallToAction'
+import Footer from '../../components/students/Footer'
 
 const Main = () => {
   return (
