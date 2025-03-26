ecfece88195ad2d389ba939f982b4de9ee8021e7
xsmartbartx <bw141105@gmail.com>
2025-03-26 15:51:46 +0100

    Create Rating.jsx

---
+++ klient/src/components/students/Rating.jsx
@@ -0,0 +1,34 @@
+import React, { use } from 'react'
+
+const Rating = () => {
+
+  const [rating, setRating] = useState(initialRating || 0);
+
+  const handleRating = (value) => {
+    setRating(value);
+    if(onRate) onRate(value);
+  }
+
+  useEffect(() => {
+    if(initialRating) {
+      setRating(initialRating);
+    }
+  }, [initialRating]);
+
+  return (
+    <div>
+      {Arrayl.from({length: 5}).map((_, index) => 
+      const starValue = index + 1;
+      return (
+        <span key={index} className={`text-x1 sm:text-2x1 cursor-pointer
+         ${starValue <= rating ? 'text-yellow-400' :
+          'text-gray-300'}`} onClick={()=> handleRating(starValue)}>
+          &#9733;
+        </span>
+      )
+      )}
+    </div>
+  )
+}
+
+export default Rating
