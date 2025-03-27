6b454b91f5c4a488412d77e442dcf22f1fe99ec5
xsmartbartx <bw141105@gmail.com>
2025-03-27 16:23:55 +0100

    Update CoursesList.jsx

--- klient/src/pages/students/CoursesList.jsx
+++ klient/src/pages/students/CoursesList.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppContext } from '../../context/AppContext'
 import { useContext } from 'react'
 import SearchBar from '../../components/uczniowie/SearchBar'
@@ -16,39 +16,56 @@ const CoursesList = () => {
 
   useEffect(() => {
     if (allCourses && allCourses.length > 0) {
-      const tempCourses = allCourses.slice()
+      const tempCourses = allCourses.slice();
 
-      input ?
-        setFilteredCourse(tempCourses.filter(
-          item => item.course.title.toLowerCase().includes(input.toLowerCase()))) :
-        
-       setFilteredCourse(tempCourses)
+      input
+        ? setFilteredCourse(
+            tempCourses.filter(item =>
+              item.course.title.toLowerCase().includes(input.toLowerCase())
+            )
+          )
+        : setFilteredCourse(tempCourses);
     }
   }, [allCourses, input]);
 
   return (
     <>
-    <div className='flex flex-col items-center space-y-5 text-center py-10'>
       <div className='flex flex-col items-center space-y-5 text-center py-10'>
-        <div>
-          <h1 className='text-4x1 font-semibold text-gray-800'>CourseList</h1>
-          <p className='text-gray-500'>
-            <span className='text-gray-600 cursor-pointer' onClick={()=> navigate('/')}>
-              Główna</span> / <span>Lista Kursów</span></p>
+        <div className='flex flex-col items-center space-y-5 text-center py-10'>
+          <div>
+            <h1 className='text-4xl font-semibold text-gray-800'>CourseList</h1>
+            <p className='text-gray-500'>
+              <span
+                className='text-gray-600 cursor-pointer'
+                onClick={() => navigate('/')}
+              >
+                Główna
+              </span>{' '}
+              / <span>Lista Kursów</span>
+            </p>
+          </div>
+          <SearchBar data={input} />
         </div>
-        <SearchBar data={input} />
+        {input && (
+          <div className='flex items-center gap-5'>
+            <p>{input}</p>
+            <img
+              src={assets.cross_icon}
+              alt=''
+              className='cursor-pointer'
+              onClick={() => navigate('/course-list')}
+            />
+          </div>
+        )}
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          {filteredCourse.map((course, index) => (
+            <CourseCard key={index} course={course} />
+          ))}
+        </div>
       </div>
-      { input && <div className='flex items-center gap-5'>
-        <p>{input}</p>
-        <img src={assets.cross_icon} alt="" className='cursor-pointer' onClick={()=> navigate('/course-list')}></img>
-        </div>}
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {filteredCourse.map((course, index) => <CourseCard key={index} course={course} />)}
-      </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default CoursesList
+export default CoursesList;
